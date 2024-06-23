ab842412c01f510ee5f0f15f8236b300fe3ec71e
Kraiwin-Namsri <kraiwinrubian@gmail.com>
2024-06-23 13:32:00 +0200

    Revert "removed build things"
    This reverts commit a49d6a463255cbfb243499e00b8d276485cb695d.

---
+++ platform/EOSM.202/version.c
@@ -0,0 +1,4 @@
+const char build_version[] = "crop_mood.2024Jun23.EOSM202";
+const char build_id[] = "NO HG";
+const char build_date[] ="2024-06-23 11:15:53 UTC";
+const char build_user[] = "winus@wingwong";
