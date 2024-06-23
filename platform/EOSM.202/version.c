a49d6a463255cbfb243499e00b8d276485cb695d
Kraiwin-Namsri <kraiwinrubian@gmail.com>
2024-06-23 13:28:13 +0200

    removed build things

--- platform/EOSM.202/version.c
+++
@@ -1,4 +0,0 @@
-const char build_version[] = "crop_mood.2024Jun23.EOSM202";
-const char build_id[] = "NO HG";
-const char build_date[] ="2024-06-23 11:15:53 UTC";
-const char build_user[] = "winus@wingwong";
